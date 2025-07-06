b406b7db38f59692de1b90bd24e6f0d827a51fbc
tettehq <ronyquash@gmail.com>
2025-07-06 16:11:23 +0000

    week 01 commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// importing namespaces for in-built methods
+using System;
+using System.Collections.Generic; // namespace for dictionary data type
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> myBio = new Dictionary<string, string>(); // create a list of key-pairs to store my info
+        myBio["name"] = "Aaron";
+        myBio["country"] = "Ghana";
+        myBio["hobbies"] = "drawing, singing, and playing football";
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine($"My name is {myBio["name"]}, and I come from {myBio["country"]}. My hobbies are {myBio["hobbies"]}");
+        Console.WriteLine("Let's have some fun together!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Overview
+
+[Software Demo Video](https://youtu.be/jWkvIeLDnp8)
+
+# Development Environment
+
+**IDE**: VS Code
+
+**Language**: C#
+
+# Useful Websites
+
+* [W3Schools](https://www.w3schools.com/cs/index.php)
+* [Microsoft Learn C# 101](https://learn.microsoft.com/en-us/training/paths/csharp-first-steps/)
